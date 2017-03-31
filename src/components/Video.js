59f65c600cc2497c504bfbfb627906d0bbe9b8fd
Roger Bassons <rogerbassons@gmail.com>
2017-03-31 19:31:47 +0200

    initial commit

---
+++ src/components/Video.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Video extends Component {
+  render() {
+    return (
+      <div>
+        <p> Video </p>
+      </div>
+    );
+  }
+}
+
+export default Video;
