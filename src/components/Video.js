e0d714f60eef04ba59a2ef0adedd1cbd2e666686
Roger Bassons <rogerbassons@gmail.com>
2017-03-31 21:18:48 +0200

    video component test

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class Video extends Component {
-  render() {
+  constructor(props) {
+   super(props)
+   this.state = {url: './mr2v2mobile.mp4'}
+ }
+
+ componentWillMount() {
+        const script = document.createElement("script");
+
+        script.src = "http://cdn.dashjs.org/latest/dash.all.min.js";
+        //script.async = true;
+
+        document.body.appendChild(script);
+}
+
+ render() {
     return (
       <div>
-        <p> Video </p>
+        <video data-dashjs-player autoplay src={this.state.url} controls></video>
       </div>
     );
   }
